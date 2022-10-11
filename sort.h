74a7ba32b5f2e87617946240966643e51a954a59
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-10-11 20:48:19 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -30,8 +30,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 /* from 3-quick_sort.c */
 void quick_sort(int *array, size_t size);
-void swap(int *array, int left, int right);
-size_t partition(int *array, int left, int right, size_t size);
 /* helper for selection_sort */
 void swap(int *a, int *b);
 
