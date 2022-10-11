441b53a55000ec829ac5a978099a2b6538c52a6a
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-10-11 14:16:14 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,7 +7,6 @@
 * Return: sorted list
 */
 
-/* the function */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp = NULL, *marker = NULL;
@@ -15,22 +14,17 @@ void insertion_sort_list(listint_t **list)
 	if ((*list)->next == NULL || list == NULL || (*list) == NULL)
 		return;
 
-	/* assign list value to temp */
 	temp = *list;
 
-	/* assign marker point in the array */
 	marker = *list;
 
 	while (marker != NULL)
 	{
 		marker = marker->next;
-		/* checks comparison from the marker to the start */
 		while (temp->prev && (temp->n < temp->prev->n))
 		{
-			/* comparison of prev node to temp next node */
 			temp->prev->next = temp->next;
 			if (temp->next != NULL)
-				/* swaps */
 				temp->next->prev = temp->prev;
 
 			temp->next = temp->prev;
@@ -40,7 +34,6 @@ void insertion_sort_list(listint_t **list)
 			else
 				*list = temp;
 			temp->next->prev = temp;
-			/* prints list after each iteration */
 			print_list(*list);
 		}
 		temp = marker;
